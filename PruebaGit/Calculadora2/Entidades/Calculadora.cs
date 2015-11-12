98a8f254d3253aa72935b93516883c96cf40ebb2
Yuuki16 <yuuki.monge16@gmail.com>
2015-11-12 09:58:34 -0600

    Primer Commit

---
+++ PruebaGit/Calculadora2/Entidades/Calculadora.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        private int numero1;
+        private int numero2;
+        private int resultado;
+
+        public int Numero1
+        {
+            get
+            {
+                return numero1;
+            }
+            set
+            {
+                this.numero1 = value;
+            }
+        }
+        public int Numero2 {
+            get {
+                return this.numero2;
+            }
+            set {
+                this.numero2 = value;
+            }
+        }
+
+        public int Resultado {
+            get {
+                return this.resultado;
+            }
+            set {
+                this.resultado = value;
+            }
+        }
+        public Calculadora() { }
+
+        public Calculadora(int p_numero1, int p_numero2, int p_resultado)
+        {
+            this.numero1 = p_numero1;
+            this.numero2 = p_numero2;
+            this.resultado = p_resultado;
+        }
+
+        public void Sumar(string p_opcion)
+        {
+            try
+            {
+                if (p_opcion == "sumar")
+                {
+                    this.resultado = this.numero1 + this.numero2;
+                }
+                else if (p_opcion == "restar")
+                {
+                    this.resultado = this.numero1 - this.numero2;
+              }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
---
+++ PruebaGit/Calculadora2/calculadora2/CalculadoraUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class CalculadoraUI
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite el primer número:");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite el segundo número:");
+            int numero2 = int.Parse(Console.ReadLine());
+            Console.Write("¿Desea sumar o restar?");
+            string opcion = Console.ReadLine();
+            Entidades.Calculadora calc = new Entidades.Calculadora();
+            calc.Numero1 = numero1;
+            calc.Numero2 = numero2;
+            calc.Sumar(opcion);
+            int resultado = calc.Resultado;
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
